c86575e2a3996bef7e47a2eadfefd58b736decc7
Mr. Algorithm <11aravipratapsingh@gmail.com>
2026-01-14 01:03:35 +0530

    update

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'http://localhost:3002/api/v1/:path*',
+        destination: `${process.env.SERVER_API_URL || 'http://localhost:3002/api/v1'}/:path*`, // Proxy to Backend
       },
     ];
   },
 };
+
 
 export default nextConfig;
